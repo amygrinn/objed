f752ec2d990257862c8abd49a10e96c78e2a025f
Tyler Grinn <tylergrinn@gmail.com>
2022-08-16 14:48:48 -0400

    Only lint project files

--- test/make-lint.el
+++ test/make-lint.el
@@ -3,43 +3,48 @@
 ;; checkdoc tests
 (require 'subr-x)
 (require 'checkdoc)
-(let ((guess-checkdoc-error-buffer-name "*Warnings*")
-      (sentence-end-double-space nil))
-  ;; This buffer name is hard-coded in checkdoc and it may change
-  (ignore-errors
-    (kill-buffer guess-checkdoc-error-buffer-name))
-  (mapc (lambda (f)
-          (checkdoc-file f))
-        (directory-files-recursively default-directory "^[^.].*\\.el$"))
-  (when-let ((buf (get-buffer guess-checkdoc-error-buffer-name)))
-    (with-current-buffer buf
-      (unless (= 0 (buffer-size))
-        (kill-emacs 1)))))
 
+(let ((files (append (directory-files default-directory nil "^[^.].*\\.el$")
+                    (directory-files-recursively
+                     (expand-file-name "deps" default-directory)
+                     "^[^.].*\\.el$"))))
 
-;; package lint
-(declare-function package-lint--get-package-prefix@objed "ext:package-lint")
-(declare-function package-lint--test-keyseq "ext:package-lint")
-(declare-function package-lint--check-eval-after-load "ext:package-lint")
-(declare-function package-lint-buffer "ext:package-lint")
-(when (require 'package-lint nil t)
-  (define-advice package-lint--get-package-prefix (:override () objed)
-    "Use objed as prefix for all files."
-    (file-name-nondirectory (expand-file-name "." default-directory)))
-  (fset #'package-lint--test-keyseq #'ignore)
-  (fset #'package-lint--check-eval-after-load #'ignore)
-  (let ((success t))
-    (dolist (file (directory-files-recursively default-directory "^[^.].*\\.el$"))
-      (with-temp-buffer
-        (insert-file-contents file t)
-        (emacs-lisp-mode)
-        (let ((checking-result (package-lint-buffer)))
-          (when checking-result
-            (setq success nil)
-            (message "In `%s':" file)
-            (pcase-dolist (`(,line ,col ,type ,message) checking-result)
-              (message "  at %d:%d: %s: %s" line col type message))))))
-    (kill-emacs (if success 0 1))))
+  (let ((guess-checkdoc-error-buffer-name "*Warnings*")
+        (sentence-end-double-space nil))
+    ;; This buffer name is hard-coded in checkdoc and it may change
+    (ignore-errors
+      (kill-buffer guess-checkdoc-error-buffer-name))
+    (mapc (lambda (f)
+            (checkdoc-file f))
+          files)
+    (when-let ((buf (get-buffer guess-checkdoc-error-buffer-name)))
+      (with-current-buffer buf
+        (unless (= 0 (buffer-size))
+          (kill-emacs 1)))))
+  
+  ;; package lint
+  (declare-function package-lint--get-package-prefix@objed "ext:package-lint")
+  (declare-function package-lint--test-keyseq "ext:package-lint")
+  (declare-function package-lint--check-eval-after-load "ext:package-lint")
+  (declare-function package-lint-buffer "ext:package-lint")
+  (when (require 'package-lint nil t)
+    (define-advice package-lint--get-package-prefix (:override () objed)
+      "Use objed as prefix for all files."
+      (file-name-nondirectory (expand-file-name "." default-directory)))
+    (fset #'package-lint--test-keyseq #'ignore)
+    (fset #'package-lint--check-eval-after-load #'ignore)
+    (let ((success t))
+      (dolist (file files)
+        (with-temp-buffer
+          (insert-file-contents file t)
+          (emacs-lisp-mode)
+          (let ((checking-result (package-lint-buffer)))
+            (when checking-result
+              (setq success nil)
+              (message "In `%s':" file)
+              (pcase-dolist (`(,line ,col ,type ,message) checking-result)
+                (message "  at %d:%d: %s: %s" line col type message))))))
+      (kill-emacs (if success 0 1)))))
 
 ;; elsa
 ;; (when (require 'elsa nil t)
