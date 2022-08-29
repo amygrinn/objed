51346e7d9f10cb740af09527a8cff0d2aa67705e
Tyler Grinn <tylergrinn@gmail.com>
2022-08-28 21:54:08 -0400

    Checking for archive contents for downloading dependencies

--- test/elpa.el
+++ test/elpa.el
@@ -3,6 +3,9 @@
 (package-initialize)
 (add-to-list 'load-path default-directory)
 
+(unless package-archive-contents
+  (package-refresh-contents))
+
 (unless (package-installed-p 'avy)
   (package-install 'avy))
 
